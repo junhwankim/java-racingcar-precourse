0891a9935db98b98df9868046440dc3a8a640cf0
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-10 21:13:26 +0900

    feat(MoveCommand): RandomNumber를 MoveCommand로 변환하는 기능 추가

--- src/main/java/racinggame/domain/MoveCommand.java
+++ src/main/java/racinggame/domain/MoveCommand.java
@@ -1,5 +1,14 @@
 package racinggame.domain;
 
 public enum MoveCommand {
-	FORWARD, STOP
+	FORWARD, STOP;
+
+	public static final int FORWARD_NUMBER = 4;
+
+	public static MoveCommand convertMoveCommand(RandomNumber randomNumber) {
+		if (randomNumber.isGreaterThanOrEqualTo(FORWARD_NUMBER))
+			return FORWARD;
+
+		return STOP;
+	}
 }
