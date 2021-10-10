2082eaed8bc27a96e9634dda17e40cf01f61b22c
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-10 16:51:41 +0900

    feat(Cars): 자동차들을 관리하기 위한 Cars 클래스 구현

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,28 @@
+package racinggame.domain;
+
+import java.util.List;
+
+public class Cars {
+	private final List<Car> cars;
+
+	public Cars(List<Car> cars) {
+		validateNotNull(cars);
+		validateCarsSize(cars);
+
+		this.cars = cars;
+	}
+
+	public List<Car> getCars() {
+		return this.cars;
+	}
+
+	private void validateNotNull(List<Car> cars) {
+		if (cars == null)
+			throw new IllegalArgumentException("[ERROR] 자동차는 2대 이상이어야 합니다.");
+	}
+
+	private void validateCarsSize(List<Car> cars) {
+		if (cars.size() < 2)
+			throw new IllegalArgumentException("[ERROR] 자동차는 2대 이상이어야 합니다.");
+	}
+}
---
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -0,0 +1,38 @@
+package racinggame.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+	@Test
+	@DisplayName("자동차 목록은 null 일 수 없다.")
+	void testCarsNotAllowedNull() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new Cars(null));
+	}
+
+	@Test
+	@DisplayName("자동차는 2대 이상이어야 한다.")
+	void testCarsSizeGreaterOrEqualsTwo() {
+		List<Car> cars = Arrays.asList(new Car("mario"));
+
+		assertThatIllegalArgumentException().isThrownBy(() -> new Cars(cars));
+	}
+
+	@Test
+	@DisplayName("Car List를 반환한다.")
+	void testGetCars() {
+		List<Car> carList = Arrays.asList(new Car("mario"), new Car("luigi"));
+
+		Cars cars = new Cars(carList);
+
+		assertThat(cars.getCars()).hasSize(2);
+		assertThat(cars.getCars().get(0).getName()).isEqualTo("mario");
+		assertThat(cars.getCars().get(1).getName()).isEqualTo("luigi");
+	}
+}
