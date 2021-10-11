30dd1dfd358f7136a3070b87836724301cf5d62e
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-11 20:09:15 +0900

    refactor(Cars): Cars를 생성하는 create 메소드를 사용하도록 변경

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,15 +1,29 @@
 package racinggame.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+	public static final String CAR_NAME_SEPARATOR = ",";
 	private final List<Car> cars;
 
-	public Cars(List<Car> cars) {
-		validateNotNull(cars);
+	private Cars(List<Car> cars) {
 		validateCarsSize(cars);
 
 		this.cars = cars;
+	}
+
+	public static Cars createCars(String carNames) {
+		validateNotNull(carNames);
+
+		String[] names = carNames.split(CAR_NAME_SEPARATOR);
+		ArrayList<Car> cars = new ArrayList<>();
+
+		for (String name: names) {
+			cars.add(new Car(name));
+		}
+
+		return new Cars(cars);
 	}
 
 	public List<Car> getCars() {
@@ -24,8 +38,8 @@ public class Cars {
 		}
 	}
 
-	private void validateNotNull(List<Car> cars) {
-		if (cars == null)
+	private static void validateNotNull(String carNames) {
+		if (carNames == null)
 			throw new IllegalArgumentException("[ERROR] 자동차는 2대 이상이어야 합니다.");
 	}
 
--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -13,41 +13,34 @@ class CarsTest {
 	@Test
 	@DisplayName("자동차 목록은 null 일 수 없다.")
 	void testCarsNotAllowedNull() {
-		assertThatIllegalArgumentException().isThrownBy(() -> new Cars(null));
+		assertThatIllegalArgumentException().isThrownBy(() -> Cars.createCars(null));
 	}
 
 	@Test
 	@DisplayName("자동차는 2대 이상이어야 한다.")
 	void testCarsSizeGreaterOrEqualsTwo() {
-		List<Car> cars = Arrays.asList(new Car("mario"));
-
-		assertThatIllegalArgumentException().isThrownBy(() -> new Cars(cars));
+		assertThatIllegalArgumentException().isThrownBy(() -> Cars.createCars("mario"));
 	}
 
 	@Test
 	@DisplayName("Car List를 얻어올 수 있다.")
 	void testGetCars() {
-		Car car1 = new Car("mario");
-		Car car2 = new Car("luigi");
-
-		Cars cars = new Cars(Arrays.asList(car1, car2));
+		Cars cars = Cars.createCars("mario,luigi");
 		List<Car> carList = cars.getCars();
 
 		assertThat(carList).hasSize(2);
-		assertThat(carList.get(0).getName()).isEqualTo(car1.getName());
-		assertThat(carList.get(1).getName()).isEqualTo(car2.getName());
+		assertThat(carList.get(0).getName()).isEqualTo("mario");
+		assertThat(carList.get(1).getName()).isEqualTo("luigi");
 	}
 
 	@Test
 	@DisplayName("자동차 이동 테스트, 자동차는 전진했거나 멈춰있다.")
 	void testMoveCars() {
-		Car car1 = new Car("mario");
-		Car car2 = new Car("luigi");
-
-		Cars cars = new Cars(Arrays.asList(car1, car2));
+		Cars cars = Cars.createCars("mario,luigi");
 		cars.moveCars();
 
-		assertThat(car1.getPosition()).isIn(0, 1);
-		assertThat(car2.getPosition()).isIn(0, 1);
+		for (Car car: cars.getCars()) {
+			assertThat(car.getPosition()).isIn(0, 1);
+		}
 	}
 }
