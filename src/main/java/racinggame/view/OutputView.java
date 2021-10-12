3b15f43a41beb45006f91b4511c4562b5dc176ab
Junhwan Kim <junhwan.tech@gmail.com>
2021-10-12 22:33:38 +0900

    feat(OutputView): 출력을 담당하는 OutputView 구현

---
+++ src/main/java/racinggame/view/OutputView.java
@@ -0,0 +1,12 @@
+package racinggame.view;
+
+import java.util.List;
+
+import racinggame.domain.CarStatus;
+import racinggame.domain.RaceResult;
+
+public class OutputView {
+	public void printMessage(String message) {
+		System.out.println(message);
+	}
+}
